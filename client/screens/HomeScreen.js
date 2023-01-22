8f3541fb9a16e6e3b63153eabf9a9a36ba1fd0f6
unknown <ahmed.asoliman1@gmail.com>
2023-01-22 20:06:31 +0200

    design searchbar

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,7 +1,7 @@
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
+import { ChevronDownIcon, UserIcon, AdjustmentsVerticalIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation()
@@ -13,11 +13,11 @@ const HomeScreen = () => {
   },[])
 
   return (
-    <SafeAreaView className="bg-white pt-5 h-20">
-      <Text className="text-red-500">
+    <SafeAreaView className="bg-white pt-5 ">
+      <View className="text-red-500">
         {/* header */}
         <View className="flex-row items-center pb-3 mx-4 space-x-2">
-          <Image source={{ uri: "https://links.papareact.com/wru" }} className="h-7 w-7 bg-gray-300 p-4 ml-4 rounded-full" />
+          <Image source={{ uri: "https://links.papareact.com/wru" }} className="h-7 w-7 bg-gray-300 p-4  rounded-full" />
           <View className="flex-1">
             <Text className="font-bold text-gray-300 text-xs">Delivery Now</Text>
             <Text className="font-bold text-xl">Current Location
@@ -26,7 +26,17 @@ const HomeScreen = () => {
           </View>
           <UserIcon size={35}/>
         </View>
-      </Text>
+        {/* searchBar */}
+        <View className="flex-row pb-2 space-x-2 mx-4 items-center">
+          <View className="flex-row bg-gray-200 p-3 space-x-2 flex-1">
+            <MagnifyingGlassIcon size={20} color="gray"/>
+            <TextInput placeholder='Find your dish' keyboardType='default'/>
+          </View>
+          <AdjustmentsVerticalIcon />
+          
+        </View>
+        
+      </View>
     </SafeAreaView>
   )
 }
